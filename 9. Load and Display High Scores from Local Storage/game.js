ed9b592087b849bb25a87ba0f4725918a805db7b
Han Weng <36247722+Han-Weng@users.noreply.github.com>
2020-08-21 11:27:57 -0400

    why usnt fetch() workking

--- 9. Load and Display High Scores from Local Storage/game.js
+++ 9. Load and Display High Scores from Local Storage/game.js
@@ -9,33 +9,23 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
-  }
-];
+
+
+let questions = [];
+
+
+
+fetch('questions.json').then(res => {
+   return res.json();
+})
+.then(loadedQuestions => {
+
+  console.log(loadedQuestions);
+  startGame();
+
+})
+;
+
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -52,7 +42,7 @@ getNewQuestion = () => {
   if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
     //go to the end page
-    return window.location.assign("/end.html");
+    return window.location.assign("end.html");
   }
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
@@ -100,5 +90,3 @@ incrementScore = num => {
   score += num;
   scoreText.innerText = score;
 };
-
-startGame();
